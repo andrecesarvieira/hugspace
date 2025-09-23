df486557a9673feea7319613c4340fed1a01ad78
André César Vieira <andrecesarvieira@hotmail.com>
2025-09-23 13:33:23 -0300

    feat: Complete database and entity cleanup for new architecture
    - Removed all existing migrations and database state
    - Deleted old entities (User, Post, Follow, PostLike, Comment)
    - Removed entity configurations from Infrastructure
    - Cleaned DbContext removing old DbSets
    - Database volumes recreated with fresh PostgreSQL instance
    - Ready for new corporate entities and migrations
    
    Breaking changes: Complete reset for new enterprise architecture

--- src/EnterpriseHub.Infrastructure/Data/EnterpriseHubDbContext.cs
+++ src/EnterpriseHub.Infrastructure/Data/EnterpriseHubDbContext.cs
@@ -1,6 +1,5 @@
 using System.Linq.Expressions;
 using EnterpriseHub.Domain.Common;
-using EnterpriseHub.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace EnterpriseHub.Infrastructure.Data;
@@ -11,12 +10,8 @@ public class EnterpriseHubDbContext : DbContext
     {
     }
 
-    // DbSets
-    public DbSet<User> Users { get; set; }
-    public DbSet<Post> Posts { get; set; }
-    public DbSet<Follow> Follows { get; set; }
-    public DbSet<PostLike> Likes { get; set; }
-    public DbSet<Comment> Comments { get; set; }
+    // DbSets - Será populado com novas entidades
+    // TODO: Adicionar novos DbSets aqui
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
